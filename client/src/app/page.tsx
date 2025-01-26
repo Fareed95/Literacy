504f06d26c0ace0f01974330502268a32e4f8434
Rehbar Khan <136853370+thisisarsh1@users.noreply.github.com>
2025-01-26 13:53:24 +0530

    Hero Spline changes

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,14 +4,20 @@ import { useEffect } from 'react';
 import Herotext from '@/components/Herotext';
 import HomeEventCard from '@/components/HomeEventCard';
 import Footer from '@/components/Footer';
-
+import Spline from '@splinetool/react-spline/next'
 
 
 export default function Home() {
 
   return (
     <div className="min-h-screen" id="root">
-      <Herotext />
+      
+    <div className='mt-[10vh]'>
+    
+      <Spline
+        scene="https://prod.spline.design/SEtXNDI9u2wUw6jp/scene.splinecode" 
+      />
+    </div>
       <HomeEventCard />
       <Footer />
     </div>
