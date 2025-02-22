006a136cd2d3b961251a0bf28c01be63a41a7206
Fareed95 <fareedsayed95@gmail.com>
2025-02-22 10:00:39 +0530

    updated the console

--- server/portfolio/views.py
+++ server/portfolio/views.py
@@ -119,3 +119,10 @@ class LinkAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ResumeAPIView(APIView):
+    def get(self, request, email):
+        user = get_object_or_404(UserDetails, email=email)
+        serializer = UserDetailsSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
